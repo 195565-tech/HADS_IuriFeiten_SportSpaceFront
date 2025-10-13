b9ca361c0aa730639dab76b746db4f8a399ab9c6
Iuri Feiten <195565@upf.br>
2025-10-13 17:37:18 -0300

    fix: authprovider

--- src/react-app/provider/AuthProvider.tsx
+++ src/react-app/provider/AuthProvider.tsx
@@ -77,6 +77,8 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       }
 
       const data = await res.json();
+            console.log('o data', data)
+
       if (data.token) {
       console.log('gravou login')
       localStorage.setItem('token', data.token);
